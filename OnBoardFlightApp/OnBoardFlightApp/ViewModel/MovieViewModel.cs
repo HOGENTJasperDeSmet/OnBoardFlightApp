223855bb2b9ffeba409ec0c27f752cc0e0d4a276
Jordi Van der Poten <jordivanderpoten@hotmail.com>
2019-12-16 14:55:07 +0100

    Movies voor mobile en tablet aangepast layout

--- OnBoardFlightApp/OnBoardFlightApp/ViewModel/MovieViewModel.cs
+++ OnBoardFlightApp/OnBoardFlightApp/ViewModel/MovieViewModel.cs
@@ -37,21 +37,27 @@ namespace OnBoardFlightApp.ViewModel
             Movies.Add(movie4);
             json = await client.GetStringAsync(new Uri("http://www.omdbapi.com/?i=tt0083658&apikey=b51adcd9"));
             var movie5 = JsonConvert.DeserializeObject<Movie>(json);
+            movie5.SourceMP4 = "ms-appx:///Assets/DummyMP4/hangover.mp4";
             Movies.Add(movie5);
             json = await client.GetStringAsync(new Uri("http://www.omdbapi.com/?i=tt0109830&apikey=b51adcd9"));
             var movie6 = JsonConvert.DeserializeObject<Movie>(json);
+            movie6.SourceMP4 = "ms-appx:///Assets/DummyMP4/hangover.mp4";
             Movies.Add(movie6);
             json = await client.GetStringAsync(new Uri("http://www.omdbapi.com/?i=tt0077869&apikey=b51adcd9"));
             var movie7 = JsonConvert.DeserializeObject<Movie>(json);
+            movie7.SourceMP4 = "ms-appx:///Assets/DummyMP4/hangover.mp4";
             Movies.Add(movie7);
             json = await client.GetStringAsync(new Uri("http://www.omdbapi.com/?i=tt0120815&apikey=b51adcd9"));
             var movie8 = JsonConvert.DeserializeObject<Movie>(json);
+            movie8.SourceMP4 = "ms-appx:///Assets/DummyMP4/hangover.mp4";
             Movies.Add(movie8);
             json = await client.GetStringAsync(new Uri("http://www.omdbapi.com/?i=tt0073195&apikey=b51adcd9"));
             var movie9 = JsonConvert.DeserializeObject<Movie>(json);
+            movie9.SourceMP4 = "ms-appx:///Assets/DummyMP4/hangover.mp4";
             Movies.Add(movie9);
             json = await client.GetStringAsync(new Uri("http://www.omdbapi.com/?i=tt0091763&apikey=b51adcd9"));
             var movie10 = JsonConvert.DeserializeObject<Movie>(json);
+            movie10.SourceMP4 = "ms-appx:///Assets/DummyMP4/hangover.mp4";
             Movies.Add(movie10);
         }
     }
